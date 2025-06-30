cd34265bb4b0f2b64a95216605bfe0ead5243bad
Veronika <>
2025-06-30 15:47:37 +0200

    Trained and saved Siamese models for different margins

--- Session6/utils/save_load.py
+++ Session6/utils/save_load.py
@@ -2,29 +2,32 @@ import os
 import torch
 
 SAVED_MODELS_ROOT = "checkpoints"
-
-def save_model(model, optimizer, epoch, stats):
-    """ Saving model checkpoint """
-    
+def get_save_root():
     if(not os.path.exists(SAVED_MODELS_ROOT)):
         os.makedirs(SAVED_MODELS_ROOT)
-    savepath = f"{SAVED_MODELS_ROOT}/checkpoint_epoch_{epoch}.pth"
+        
+    return SAVED_MODELS_ROOT
+
+def save_model(model, name, stats = {}, save_path = get_save_root()):
+    """ Saving model checkpoint """
+    
+    if(not os.path.exists(save_path)):
+        os.makedirs(save_path)
+    savepath = f"{save_path}/{name}.pth"
 
     torch.save({
-        'epoch': epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'stats': stats
-    }, savepath)
+            "model_state_dict": model.state_dict(),
+            "stats": stats,
+        }, savepath)
+
+    print(f"Saved state to {savepath}")
     
     return savepath
 
-def load_model(model, optimizer, savepath = SAVED_MODELS_ROOT):
+def load_model(model, name, savedir = get_save_root()):
     """ Loading pretrained checkpoint """
-    checkpoint = torch.load(savepath)
+    checkpoint = torch.load(f"{savedir}/{name}.pth")
     model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    epoch = checkpoint["epoch"]
     stats = checkpoint["stats"]
     
-    return model, optimizer, epoch, stats
+    return model, stats
